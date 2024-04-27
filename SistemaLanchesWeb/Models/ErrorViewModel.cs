3e53c9bd7beb505f6ae29d5727832729656898b5
Rafael B <rafaeltec_@live.com>
2024-04-27 15:31:29 -0300

    Envio do projeto inicial ao GitHub

---
+++ SistemaLanchesWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SistemaLanchesWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
