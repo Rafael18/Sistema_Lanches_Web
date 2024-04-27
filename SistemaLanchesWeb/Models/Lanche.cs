d81bc29dc8340ca75f99587277e6f52e5e697615
Rafael B <rafaeltec_@live.com>
2024-04-27 16:22:04 -0300

    Criação das classes de modelo e Carousel na view Home/index

---
+++ SistemaLanchesWeb/Models/Lanche.cs
@@ -0,0 +1,19 @@
+﻿namespace SistemaLanchesWeb.Models
+{
+    public class Lanche
+    {
+        public int LancheId { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public string DescricaoDetalhada { get; set; }
+        public decimal Preco { get; set; }
+        public string ImagemUrl { get; set; }
+        public string ImagemThumbnailUrl { get; set; }
+        public bool IsLanchePreferido { get; set; }
+        public bool EmEstoque { get; set; }
+
+        //Relação entre as tabelas Lanche x Gategoria
+        public int CategoriaId { get; set; }
+        public virtual Categoria Categoria { get; set; }
+    }
+}
